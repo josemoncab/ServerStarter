c6e04f0da94f9553e4d2d6f242795b7a6e9ae177
josemc <josemoncab2001@gmail.com>
2023-10-15 18:08:09 +0200

    Add logger utils

---
+++ src/logger.rs
@@ -0,0 +1,23 @@
+pub fn debug(msg: &str) {
+    println!("[\x1b[37mDEBUG\x1b[0m] {msg}");
+}
+
+pub fn user(msg: &str) {
+    println!("[\x1b[95mUSER\x1b[0m] {msg}")
+}
+
+pub fn success(msg: &str) {
+    println!("[\x1b[92mSUCCESS\x1b[0m] {msg}")
+}
+
+pub fn info(msg: &str) {
+    println!("[\x1b[94mINFO\x1b[0m] {msg}")
+}
+
+pub fn warn(msg: &str) {
+    println!("[\x1b[33mWARN\x1b[0m] {msg}")
+}
+
+pub fn error(msg: &str) {
+    println!("[\x1b[31mERROR\x1b[0m] {msg}")
+}
